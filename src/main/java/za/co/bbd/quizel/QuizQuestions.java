aa39cb71069c2bef21e84416ec1574ef1f09a00c
Gabriel da Silva <gabrield@bbd.co.za>
2023-03-17 07:21:49 +0200

    QuizQuestions class creation with getters and setters

---
+++ src/main/java/za/co/bbd/quizel/QuizQuestions.java
@@ -0,0 +1,36 @@
+package za.co.bbd.quizel;
+
+public class QuizQuestions {
+    public String Question;
+    public String Answer;
+    public String[] Options;
+    public QuizQuestions(String ques, String ans, String[] opt){
+        Question = ques;
+        Answer = ans;
+        Options = opt;
+    }
+
+    public String getQuestion() {
+        return Question;
+    }
+
+    public void setQuestion(String question) {
+        Question = question;
+    }
+
+    public String getAnswer() {
+        return Answer;
+    }
+
+    public void setAnswer(String answer) {
+        Answer = answer;
+    }
+
+    public String[] getOptions() {
+        return Options;
+    }
+
+    public void setOptions(String[] options) {
+        Options = options;
+    }
+}
