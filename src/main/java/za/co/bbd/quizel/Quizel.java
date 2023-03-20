89fd8ccd87a2f7eea446f99c916665c2b73c6542
Ethan Alberts <ethana@bbd.co.za>
2023-03-20 08:30:29 +0200

    Added commands, added a simple game loop, updated pom file to use version 19 of the JDK

--- src/main/java/za/co/bbd/quizel/Quizel.java
+++ src/main/java/za/co/bbd/quizel/Quizel.java
@@ -1,10 +1,18 @@
 package za.co.bbd.quizel;
 
+import java.util.Scanner;
+
 public class Quizel
 {
     public static void main( String[] args )
     {
         // TODO: Start game loop
+        Commands userInputs = new Commands();
         System.out.println( "Welcome to quizel!" );
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            userInputs.ProcessInput(sc.nextLine());
+        }
     }
 }
