196fc42e0fc020266bff822888ef36b64eb746ab
Ethan Alberts <ethana@bbd.co.za>
2023-03-23 10:16:31 +0200

    saving

--- src/main/java/za/co/bbd/quizel/Commands.java
+++ src/main/java/za/co/bbd/quizel/Commands.java
@@ -1,29 +1,125 @@
 package za.co.bbd.quizel;
 
+import za.co.bbd.quizel.models.Genre;
+import za.co.bbd.quizel.models.QuizQuestion;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Commands {
-    public Commands() {
+    List<Genre> categories;
+    int countTotal = 0;
+    int countCorrect = 0;
+    int countWrong = 0;
+    int count = 1;
+
+    public Commands(List<Genre> genre) {
+        this.categories = genre;
     }
-    public static void ProcessInput(String input){
+    public void ProcessInput(String input, QuizQuestion... questions){
         String userInput = input.toLowerCase().trim();
         if (userInput.equals("quit") || userInput.equals("q")) {
             CloseProgram();
         } else if (userInput.equals("A") || userInput.equals("B") || userInput.equals("C") || userInput.equals("D")) {
-            ProcessUserAnswer(userInput);
-        } else {
+            ProcessUserAnswer(userInput, questions[0]);
+        }else if(userInput.equals("menu") || userInput.equals("m")){
+            Menu();
+        }else if(Integer.parseInt(input) < this.categories.size() && Integer.parseInt(input) > 1){
+            questions(input);
+        }
+        else {
             ListCommands();
         }
 
     }
 
-    public static void ListCommands(){
+    private static void ListCommands(){
         System.out.println("Place Holder");
     }
 
-    public static void ProcessUserAnswer(String answer){
-        return;
+    private void ProcessUserAnswer(String userAnswer, QuizQuestion q){
+        char ans;
+        ans = userAnswer.charAt(0);
+
+        String answer = "";
+
+        switch (ans) {
+            case 'A' -> answer = q.options().get(0);
+            case 'B' -> answer = q.options().get(1);
+            case 'C' -> answer = q.options().get(2);
+            case 'D' -> answer = q.options().get(3);
+            default -> {
+            }
+        }
+
+        if(answer.equals(q.answer()))
+        {
+            System.out.println("------------------------------------------------------");
+            System.out.println("                  Correct Answer                      ");
+            System.out.println("------------------------------------------------------");
+            countCorrect++;
+        }
+        else
+        {
+            System.out.println("------------------------------------------------------");
+            System.out.println("                  Wrong Answer                      ");
+            System.out.println("------------------------------------------------------");
+            countWrong++;
+        }
     }
 
-    public static void CloseProgram(){
+    private static void CloseProgram(){
         System.exit(0);
     }
+
+    private void Menu(){
+        System.out.println("------------------------------------------------------");
+        System.out.println( "                  Choose a category!" );
+        System.out.println("------------------------------------------------------");
+
+        this.count = 1;
+
+        for (Genre genreOption : this.categories){
+            System.out.println(this.count + ". " + genreOption.GenreDescription());
+            this.count++;
+        }
+        System.out.println(this.count + ". " + "Random\n");
+        System.out.println( "[M]enu: Go to menu                  [Q]uit: Quit game" ); // Handle user input
+    }
+
+    private void questions(String userGenre){
+        this.countTotal = 0;
+        this.countCorrect = 0;
+        this.countWrong = 0;
+
+//        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        List<QuizQuestion> questions;
+//        questions = this.categories.get(rand.nextInt(this.categories.size()-1)).GenreQuestions();
+//        System.out.println("============================================================================================");
+//        System.out.println( "Begin " +  this.categories.get(rand.nextInt(this.categories.size()-1)).GenreDescription() + " Quiz!");
+//        System.out.println("============================================================================================");
+        questions = this.categories.get(Integer.parseInt(userGenre)-1).GenreQuestions();
+        System.out.println("============================================================================================");
+        System.out.println( "Begin " +  this.categories.get(Integer.parseInt(userGenre)-1).GenreDescription() + " Quiz!");
+        System.out.println("============================================================================================");
+
+        countTotal = questions.size();
+
+        for(QuizQuestion q: questions){
+            System.out.println(q.question());
+            System.out.println("A : " +q.options().get(0));
+            System.out.println("B : " +q.options().get(1));
+            System.out.println("C : " +q.options().get(2));
+            System.out.println("D : " +q.options().get(3));
+
+            ProcessInput(sc.nextLine(),q); //Handle user input
+
+            System.out.println("============================================================================================");
+        }
+        Results result = new Results(countTotal,countCorrect, countWrong);
+        result.showResult();
+    }
 }
+
--- src/main/java/za/co/bbd/quizel/Quizel.java
+++ src/main/java/za/co/bbd/quizel/Quizel.java
@@ -24,98 +24,20 @@ public class Quizel
 
     void begin( ) {
         // TODO: Start game loop
-        Commands userInputs = new Commands();
-        System.out.println( "Welcome to quizel!" );
+        List<Genre> data = JsonDataMapper.getAllData();
+        Commands userInputs = new Commands(data);
         Scanner sc = new Scanner(System.in);
 
-        int countTotal = 0;
-        int countCorrect = 0;
-        int countWrong = 0;
 
         // TODO: Handle user input
-        List<Genre> data = JsonDataMapper.getAllData();
 
         System.out.println("============================================================================================");
         System.out.println( "                  Welcome to Quizel!" );
         System.out.println("------------------------------------------------------");
-        System.out.println( "                  Choose a category!" );
-        System.out.println("------------------------------------------------------");
 
-        int count = 1;
-        for (Genre genreOption : data){
-            System.out.println(count + ". " + genreOption.GenreDescription());
-            count++;
+        while(true){
+            System.out.println( "[M]enu: Go to menu                  [Q]uit: Quit game" );
+            userInputs.ProcessInput(sc.nextLine()); //handle user input
         }
-        System.out.println(count + ". " + "Random");
-
-        System.out.println();
-        System.out.println( "[M]enu: Go to menu                  [Q]uit: Quit game" ); // Handle user input
-        boolean temp = true;
-        Random rand = new Random();
-
-        while(temp){
-//            userInputs.ProcessInput(sc.nextLine());
-            String userGenre = sc.nextLine(); //handle user input
-            List<QuizQuestion> questions;
-            if (count == Integer.parseInt(userGenre)){
-                questions = data.get(rand.nextInt(data.size()-1)).GenreQuestions();
-                System.out.println("============================================================================================");
-                System.out.println( "Begin " +  data.get(rand.nextInt(data.size()-1)).GenreDescription() + " Quiz!");
-                System.out.println("============================================================================================");
-            } else {
-                questions = data.get(Integer.parseInt(userGenre)-1).GenreQuestions();
-                System.out.println("============================================================================================");
-                System.out.println( "Begin " +  data.get(Integer.parseInt(userGenre)-1).GenreDescription() + " Quiz!");
-                System.out.println("============================================================================================");
-            }
-
-            countTotal = questions.size();
-
-            for(QuizQuestion q: questions){
-                System.out.println(q.question());
-                System.out.println("A : " +q.options().get(0));
-                System.out.println("B : " +q.options().get(1));
-                System.out.println("C : " +q.options().get(2));
-                System.out.println("D : " +q.options().get(3));
-
-                // userInputs.ProcessInput(sc.nextLine()); //Handle user input
-                String userAnswer = sc.nextLine();
-
-                char ans;
-                ans = userAnswer.charAt(0);
-
-                String answer = "";
-
-                switch (ans) {
-                    case 'A' -> answer = q.options().get(0);
-                    case 'B' -> answer = q.options().get(1);
-                    case 'C' -> answer = q.options().get(2);
-                    case 'D' -> answer = q.options().get(3);
-                    default -> {
-                    }
-                }
-
-                if(answer.equals(q.answer()))
-                {
-                    System.out.println("------------------------------------------------------");
-                    System.out.println("                  Correct Answer                      ");
-                    System.out.println("------------------------------------------------------");
-                    countCorrect++;
-                }
-                else
-                {
-                    System.out.println("------------------------------------------------------");
-                    System.out.println("                  Wrong Answer                      ");
-                    System.out.println("------------------------------------------------------");
-                    countWrong++;
-                }
-
-                System.out.println("============================================================================================");
-            }
-            temp= !temp;
-        }
-
-        Results result = new Results(countTotal,countCorrect, countWrong);
-        result.showResult();
     }
 }
